a7503f6153ba715c5c00314fab7e7dff74e1f429
kamataryo <kamataryo@users.noreply.github.com>
2020-08-09 22:22:01 +0900

    Add receipt download

--- src/components/Team/Billing/Receipts.tsx
+++ src/components/Team/Billing/Receipts.tsx
@@ -5,6 +5,7 @@ import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
+import IconDownload from "@material-ui/icons/CloudDownload";
 import moment from "moment";
 import { __ } from "@wordpress/i18n";
 
@@ -20,38 +21,41 @@ const useInvoices = (props: Props) => {
   const { session, teamId } = props;
   const [loaded, setLoaded] = React.useState(false);
   const [invoices, setInvoices] = React.useState<Geolonia.Invoice[]>([]);
+  const [charges, setCharges] = React.useState<Geolonia.Charge[]>([]);
 
   // チーム変えたらロード状態をリセット
   React.useEffect(() => {
     setLoaded(false);
     setInvoices([]);
+    setCharges([]);
   }, [teamId]);
 
   React.useEffect(() => {
     if (loaded) {
       return;
     } else {
-      fetch(
-        session,
-        `https://api.app.geolonia.com/${process.env.REACT_APP_STAGE}/teams/${teamId}/invoices`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json"
-          }
+      const headers = {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+      };
+      const urlBase = `https://api.app.geolonia.com/${process.env.REACT_APP_STAGE}/teams/${teamId}`;
+      const handleResponse = (res: Response) => {
+        if (res.status < 400) {
+          return res.json();
+        } else {
+          console.error(res);
+          // throw new Error();
         }
-      )
-        .then(res => {
-          if (res.status < 400) {
-            return res.json();
-          } else {
-            console.error(res);
-            // throw new Error();
-          }
-        })
-        .then(result => {
-          if (result) {
-            setInvoices(result.data);
+      };
+
+      Promise.all([
+        fetch(session, `${urlBase}/invoices`, headers).then(handleResponse),
+        fetch(session, `${urlBase}/charges`, headers).then(handleResponse)
+      ])
+        .then(([invoices, charges]) => {
+          if (invoices && charges) {
+            setInvoices(invoices.data);
+            setCharges(charges.data);
           }
         })
         .catch(error => {
@@ -61,23 +65,21 @@ const useInvoices = (props: Props) => {
           setLoaded(true);
         });
     }
-  }, [session, teamId, loaded, invoices]);
+  }, [session, teamId, loaded, invoices, charges]);
 
-  return { invoices, loaded };
+  return { invoices, charges, loaded };
 };
 
 function PaymentHistory(props: Props) {
-  const { invoices, loaded } = useInvoices(props);
+  const { invoices, charges, loaded } = useInvoices(props);
   const formatter = (currency: string) =>
-    new Intl.NumberFormat(props.language, {
-      style: "currency",
-      currency
-    });
-
+    new Intl.NumberFormat(props.language, { style: "currency", currency });
+  if (!loaded && invoices.length === 0) {
+    return null;
+  }
   if (loaded && invoices.length === 0) {
     return <p>{__("No payment history.")}</p>;
   }
-
   // const currentBalance =
   //   invoices[0] && invoices[0].ending_balance !== null
   //     ? formatter(invoices[0].currency).format(
@@ -87,14 +89,6 @@ function PaymentHistory(props: Props) {
   return (
     <Table className="payment-info">
       <TableBody>
-        <TableRow>
-          <TableCell component="th" scope="column">
-            {__("Date")}
-          </TableCell>
-          <TableCell component="th" scope="column">
-            {__("Payment")}
-          </TableCell>
-        </TableRow>
         {invoices
           .map(invoice => {
             const {
@@ -103,7 +97,8 @@ function PaymentHistory(props: Props) {
               period_start,
               ending_balance,
               starting_balance,
-              id
+              id,
+              descriptions
             } = invoice;
 
             // const formattedTotal = new Intl.NumberFormat(props.language, {
@@ -117,21 +112,41 @@ function PaymentHistory(props: Props) {
             //   ((ending_balance || 0) - starting_balance) / 100
             // );
 
+            const charge = charges.find(charge => charge.invoice === id);
+
             return {
               date: moment(period_start * 1000).format("YYYY-MM-DD"),
               total,
               payment: formattedActualPayment,
               id,
+              descriptions,
+              receipt_url: charge && charge.receipt_url,
               value
             };
           })
           .filter(data => data.total > 0 && data.value > 0)
           .map(data => {
-            const { id, date, payment } = data;
+            const { id, date, payment, receipt_url, descriptions } = data;
             return (
               <TableRow key={id}>
                 <TableCell>{date}</TableCell>
+                <TableCell>
+                  <ul>
+                    {descriptions
+                      .filter(x => !!x)
+                      .map((description, index) => (
+                        <li key={index}>{description}</li>
+                      ))}
+                  </ul>
+                </TableCell>
                 <TableCell>{payment}</TableCell>
+                <TableCell>
+                  {receipt_url && (
+                    <a href={receipt_url}>
+                      <IconDownload></IconDownload>
+                    </a>
+                  )}
+                </TableCell>
               </TableRow>
             );
           })}
